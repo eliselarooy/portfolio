49512fdcdbb337ea4ccbba545b4fe2155311194d
Elise La Rooy <eliselarooy@gmail.com>
2022-05-12 18:11:04 +0100

    Updated links and font

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,16 @@
 const menuBtn = document.querySelector('.menu-btn');
 const navLinks = document.querySelector('.navbar-links');
+const toTop = document.querySelector('.to-top');
 
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('open');
   navLinks.classList.toggle('active');
 });
+
+window.onscroll = function () {
+  if (window.scrollY >= 500) {
+    toTop.style.opacity = '1';
+  } else {
+    toTop.style.opacity = '0';
+  }
+};
